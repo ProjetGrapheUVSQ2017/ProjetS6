777efcbd78725b4f85b535f7e772d5da7b9b732f
ElAssriAziz <aziz.elassri3@gmail.com>
2017-04-25 10:07:29 +0100

    Update Variable.java

--- src/Variable.java
+++ src/Variable.java
@@ -1,4 +1,27 @@
 
-public class Variable {
 
+public abstract class Variable {
+  
+private int id;
+private boolean poids;
+  
+public int getId(){
+	return this.id;
 }
+public void setId(int id){
+	this.id=id;
+}
+public abstract int getTypeVar();
+public abstract String toString();
+public boolean isPoids(){
+	if(this.poids)
+		return true;
+	else
+		return false;
+}
+public void setPoids(boolean poids){
+	this.poids=poids;
+}
+  
+}
+
