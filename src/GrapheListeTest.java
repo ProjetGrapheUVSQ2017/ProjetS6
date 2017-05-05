ae50373c2bc1459d46f3bf6c6fa9c3b0134abaa3
Nambrok <spaceship84@gmail.com>
2017-05-05 19:49:29 +0200

    Ajout de quelques tests, quelques problème sur le test de suppression d'arc

--- src/GrapheListeTest.java
+++ src/GrapheListeTest.java
@@ -21,6 +21,12 @@ public class GrapheListeTest {
 	}
 	
 	@Test
+	public void testAjoutsommetNull(){
+		graphe.addSommet((Sommet)null);
+		assertTrue("Sommet null ajouté", graphe.getNbSommets() == 0);
+	}
+	
+	@Test
 	public void testAjoutSommet(){
 		graphe.addSommet(new Sommet(new Point(10, 11)));
 		assertTrue("Sommet crÃ©e incorrectement x", graphe.getSommet(0).getPoint().x == 10);
@@ -40,6 +46,22 @@ public class GrapheListeTest {
 		assertTrue("Le sommet s n'existe pas", s != null);
 		graphe.addArc(s, s);
 		assertTrue("L'arc (s,s) n'existe pas.", graphe.existArc(s, s));
+	}
+	
+	@Test
+	public void testSuppressionArc(){
+		graphe.addSommet(new Point(10, 11));
+		graphe.addSommet(new Point(12, 13));
+		
+		Sommet d = graphe.getSommet(0);
+		Sommet a = graphe.getSommet(1);
+		
+		assertFalse("Sommet départ et sommet arrivée sont les mêmes", d.equals(a));
+		
+		graphe.addArc(d, a);
+		assertTrue("Arc non crée correctement", graphe.existArc(d, a));
+		graphe.deleteArc(d, a);
+		assertFalse("Arc supprimé correctement", graphe.existArc(d, a));
 	}
 
 }
