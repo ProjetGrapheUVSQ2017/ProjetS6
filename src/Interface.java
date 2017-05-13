adb5bd939bcfc0580b6ed480336f6cb9c1058019
Maxence Joulin <maxence.joulin@gmail.com>
2017-05-13 17:34:55 +0200

    Les arcs ne sont plus dessiner dans les cercles des sommets et ajout de la variable rayon_sommet

--- src/Interface.java
+++ src/Interface.java
@@ -13,6 +13,7 @@ public class Interface extends JComponent {
     private boolean selectionEnCours;
     private MenuPanel control = new MenuPanel();
     private Graphe graphe;
+    private int rayon_sommet = 15;
 
     public static void main(String[] args) throws Exception{
         JFrame f = new JFrame("Graphe");
@@ -57,7 +58,7 @@ public class Interface extends JComponent {
 
         for(Sommet s : graphe.get_liste_de_sommet()){
             g.setColor(s.getCouleur());
-            g.fillOval(s.getPoint().x-10, s.getPoint().y-10, 20,20);
+            g.fillOval(s.getPoint().x-rayon_sommet, s.getPoint().y-rayon_sommet, rayon_sommet*2,rayon_sommet*2);
         }
         for(Arc a : graphe.get_liste_arc()){
             g.setColor(a.getCouleur());
@@ -78,13 +79,13 @@ public class Interface extends JComponent {
 
         double dx = x2 - x1, dy = y2 - y1;
         double angle = Math.atan2(dy, dx);
-        int len = (int) Math.sqrt(dx*dx + dy*dy);
+        int len = (int) Math.sqrt(dx*dx + dy*dy) - rayon_sommet;
         AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
         at.concatenate(AffineTransform.getRotateInstance(angle));
         g.transform(at);
 
         // Draw horizontal arrow starting in (0, 0)
-        g.drawLine(0, 0, len, 0);
-        g.fillPolygon(new int[] {len-10, len-ARR_SIZE-10, len-ARR_SIZE-10, len-10}, new int[] {0, -ARR_SIZE/2, ARR_SIZE/2, 0}, 4);
+        g.drawLine(rayon_sommet, 0, len, 0);
+        g.fillPolygon(new int[] {len, len-ARR_SIZE, len-ARR_SIZE, len}, new int[] {0, -ARR_SIZE/2, ARR_SIZE/2, 0}, 4);
     }
 }
