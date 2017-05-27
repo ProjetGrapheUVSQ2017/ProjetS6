f5081036b3014885381a89e30ea29840547563d9
Nambrok <spaceship84@gmail.com>
2017-05-27 20:01:03 +0200

    ajout de commentaires et javadoc

--- src/VarFloat.java
+++ src/VarFloat.java
@@ -1,33 +1,64 @@
-
+/**
+ * Variable implémentant un flottant
+ * @author Aziz
+ *
+ */
 public class VarFloat extends Variable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8699786705793512893L;
+	/**
+	 * Valeur de la variable
+	 */
 	private float val;
+	
+	/**
+	 * Constructeur de la variable
+	 * @param val valeur d'origine de la variable
+	 */
 	public VarFloat(float val){
 		this.val=val;
 	}
+	
+	/**
+	 * Getter de la variable
+	 * @return float
+	 */
 	@Override
 	public float getFloat(){
 		return this.val;
 	}
+	
+	/**
+	 * Setter de la valeur flottante
+	 * @param val
+	 */
 	public void setFloat(float val){
 		 this.val=val;
 	}
+	
+	/**
+	 * Renvoie le type de la variable sous forme de string.
+	 * Permet l'affichage du type dans l'interface
+	 * @return String
+	 */
 	@Override
 	public String getTypeVar() {
 		return "Float";
 	}
 
+	/**
+	 * Renvoie la valeur sous forme de String pour l'affichage
+	 */
 	@Override
 	public String toString() {
 		return Float.toString(val);
 	}
+	
+	
 	@Override
 	public int getInt() {
 		return 0;
 	}
+	
 	@Override
 	public String getString() {
 		return null;
--- src/VarInt.java
+++ src/VarInt.java
@@ -1,34 +1,64 @@
 
+/**
+ * Implémentation de la variable avec une valeur entière
+ * @author Aziz
+ *
+ */
 public class VarInt extends Variable{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 5594894313407954692L;
+	/**
+	 * Valeur
+	 */
 	private int val;
+	
+	/**
+	 * Constructeur de VarInt
+	 * @param val int
+	 */
 	public  VarInt(int val){
 		this.val=val;
 	}
+	
+	/**
+	 * Getter pour la valeur
+	 * @return int
+	 */
 	@Override
 	public int getInt(){
 		return this.val;
 	}
+	
+	/**
+	 * Setter de la valeur
+	 * @param val nouvelle valeur
+	 */
 	public void setInt(int val){
 		this.val=val;
 	}
+	
+	/**
+	 * Renvoie le type de la variable sous forme de String pour l'affichage des variables
+	 * @return String
+	 */
 	@Override
 	public String getTypeVar() {
 		return "Int";
 	}
 
+	
 	@Override
 	public String toString() {
 		return Integer.toString(val);
 	}
+	
+	
 	@Override
 	public String getString() {
 		return null;
 	}
+	
+	
 	@Override
 	public float getFloat() {
 		return 0;
